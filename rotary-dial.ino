803a6adcd96abd3df047d1649a2a0732c6dcaa73
Alexandr Alakin <alakinalexandr@gmail.com>
2018-03-27 11:31:54 +0600

    LED turns off after ledTimeout (tinkercad.com)
    Checked at tinkercad.com
    Renamed some variables

--- rotary-dial.ino
+++ rotary-dial.ino
@@ -6,17 +6,19 @@
 #define DIAL_PIN   A2  // Pin for normally opened rotary dialer
 
 LiquidCrystal lcd(8,7,5,4,3,2);
-int count = 0;               // Current digit
-int pos = 0;                 // Current position for LCD
-char count_char[2];          // Char to print digit on LCD
+int count = 0;            // Current digit
+int pos = 0;              // Current position for LCD
+char count_char[2];       // Char to print digit on LCD
 
-bool reading = false;        // Current reading of state
-bool lastRead = false;       // Previous reading
-bool lcd_on = false;         // True if LCD has any digits
-long lastChangeTime = 0;     // Time when last change happened
-int debounceDelay = 10;      // Minimum time between short circuits
-int interDigitTimeout = 200; // Minimum time between digits
-int idleTimeout = 5000;      // Time before turning LCD off
+bool reading = false;     // Current reading of state
+bool lastRead = false;    // Previous reading
+bool lcd_led_on = false;  // True if LCD has LED on
+bool lcd_in_use = false;  // True if LCD has non-default text
+long lastChangeTime = 0;  // Time when last change happened
+int debounceDelay = 10;   // Minimum time between short circuits
+int digitTimeout = 100;   // Minimum time between digits
+int textTimeout = 1000;   // Time before deleting non-default text
+int ledTimeout =  2000;   // Time before turning LED off
 
 void setup()
 {
@@ -24,8 +26,8 @@ void setup()
   pinMode(DIAL_PIN, INPUT_PULLUP);
   pinMode(LCD_LED, OUTPUT);
   pinMode(LCD_BRIGHT, OUTPUT);
-  analogWrite(LCD_LED, 255);
-  analogWrite(LCD_BRIGHT, 80);
+  analogWrite(LCD_LED, 0);
+  analogWrite(LCD_BRIGHT,30);
   lcd.begin(16, 2);
   reset_lcd();
 }
@@ -38,29 +40,37 @@ void reset_lcd()
 
 void loop()
 {
-  if ( lcd_on ) {
-    // Bother only if lcd is on
-    if ( (millis() - lastChangeTime) > idleTimeout) {
-      // Idle timeout expired, so reset
-      Serial.println("Idle timeout expired");
-      reset_lcd();
-      pos = 0;
-      lcd_on = false;
-    }
-
-    if ( (millis() - lastChangeTime) > interDigitTimeout) {
-      // interDigitTimeout has passed
-      if ( count ) {
-        // Some number was dialed, so change position
-        Serial.println("done");
-        count = 0;                // Reset digit
-        pos++;                    // Move to the right
-        if ( pos > 15 ) {         // Carriage return if number is too long
-          pos = 0;
-        }
-        lcd.setCursor(pos,1);
-        lcd.print(" ");
+  if ( lcd_led_on ) {
+    // Bother only if LCD's LED is on
+    if ( lcd_in_use ) {
+      // Bother only if LCD has some non-default text
+      if ( (millis() - lastChangeTime) > textTimeout) {
+        // Idle timeout expired, so reset
+        Serial.println("Text timeout expired");
+        reset_lcd();
+        pos = 0;
+        lcd_in_use = false;
       }
+
+      if ( (millis() - lastChangeTime) > digitTimeout) {
+        // interDigitTimeout has passed
+        if ( count ) {
+          // Some number was dialed, so change position
+          Serial.println("done");
+          count = 0;                // Reset digit
+          pos++;                    // Move to the right
+          if ( pos > 15 ) {         // Carriage return if number is too long
+            pos = 0;
+          }
+          lcd.setCursor(pos,1);
+          lcd.print(" ");
+        }
+      }
+    } else if ( (millis() - lastChangeTime) > ledTimeout) {
+      // if LCD LED timeout expired, then turn LED off
+      Serial.println("LED timeout expired");
+      lcd_led_on = false;
+      analogWrite(LCD_LED,0);
     }
   }
 
@@ -71,7 +81,8 @@ void loop()
       lastChangeTime = millis();          // Keep track of time
       lastRead = reading;                 // Keep track of readings
       if ( !reading ) {                   // If circuit has opened
-        lcd_on = true;
+        lcd_in_use = lcd_led_on = true;
+        analogWrite(LCD_LED,255);
         count++;                          // Increment digit
         itoa(count % 10, count_char, 10); // Integer to char
         lcd.setCursor(pos,1);
